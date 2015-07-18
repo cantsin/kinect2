8f51fe6dac7293218527126b3a672202c23c028f
James Tranovich <jtranovich@gmail.com>
2015-07-18 13:09:30 -0700

    Fix skeleton example.

--- examples/skeletons-browser/index.js
+++ examples/skeletons-browser/index.js
@@ -1,23 +1,31 @@
 var Kinect2 = require('../../kinect2'),
-	express = require('express'),
-	app = express(),
-	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+    express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
 
 var kinect = new Kinect2();
 
 if(kinect.open()) {
-	server.listen(8000);
-	console.log('Server listening on port 8000');
-	console.log('Point your browser to http://localhost:8000');
+    server.listen(8000);
+    console.log('Server listening on port 8000');
+    console.log('Point your browser to http://localhost:8000');
 
-	app.get('/', function(req, res) {
-		res.sendFile(__dirname + '/public/index.html');
-	});
+    // app.get('/', function(req, res) {
+    //     res.sendFile(__dirname + '/public/index.html');
+    // });
+    app.use(express.static(__dirname + '/public'));
 
-	kinect.on('bodyFrame', function(bodies){
-		io.sockets.emit('bodyFrame', bodies);
-	});
+    kinect.on('bodyFrame', function(bodies){
+	io.sockets.emit('bodyFrame', bodies);
+    });
 
-	kinect.openBodyReader();
+    // XXX: just flush the damn socket?
+    io.on("connection", function(socket) {
+        socket.on("refresh", function(name, fn) {
+            fn(name);
+        });
+    });
+
+    kinect.openBodyReader();
 }
